d76440bfaf1a1e7c12839ce8acb8edb5a2865f94
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-09-09 13:47:52 +0200

    nav base functionality added

--- Resources/Scripts/schedule.ts
+++ Resources/Scripts/schedule.ts
@@ -1,3 +1,20 @@
 export function initSchedule(){
-    
+    const navButtons = document.querySelectorAll('.nav__button')
+    const scheduleDays = document.querySelectorAll('.schedule')
+
+    for (let i = 0; i < scheduleDays.length; i++){
+        const button = navButtons[i]
+        const day = scheduleDays[i]
+
+        button.addEventListener('click', () => {
+            scheduleDays.forEach(day => {
+                day.classList.remove('schedule--active')
+            })
+            navButtons.forEach(button => {
+                button.classList.remove('nav__button--active')
+            })
+            button.classList.add('nav__button--active')
+            day.classList.add('schedule--active')
+        })
+    }
 }
--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -12,6 +12,22 @@
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.initSchedule = void 0;
 function initSchedule() {
+    const navButtons = document.querySelectorAll('.nav__button');
+    const scheduleDays = document.querySelectorAll('.schedule');
+    for (let i = 0; i < scheduleDays.length; i++) {
+        const button = navButtons[i];
+        const day = scheduleDays[i];
+        button.addEventListener('click', () => {
+            scheduleDays.forEach(day => {
+                day.classList.remove('schedule--active');
+            });
+            navButtons.forEach(button => {
+                button.classList.remove('nav__button--active');
+            });
+            button.classList.add('nav__button--active');
+            day.classList.add('schedule--active');
+        });
+    }
 }
 exports.initSchedule = initSchedule;
 
